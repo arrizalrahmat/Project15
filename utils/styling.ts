924d325aa0cecba49b0ef692ac20d3674495d9d4
arrizal <arrizalrahmat@gmail.com>
2023-05-04 21:09:37 +0700

    change toggle color from green to blue

--- utils/styling.ts
+++ utils/styling.ts
@@ -3,11 +3,11 @@ import {Platform} from 'react-native';
 export const chooseOS = () => {
   if (Platform.OS === 'ios') {
     return {
-      backgroundColor: 'green',
+      backgroundColor: 'blue',
     };
   } else {
     return {
-      backgroundColor: 'red',
+      backgroundColor: 'yellow',
     };
   }
 };
